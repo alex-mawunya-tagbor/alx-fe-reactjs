a613021e7851aeae264153ef45d3bf4a498d4a1b
Alex-Mawunya-Tagbor <alexm8761@gmail.com>
2025-08-30 17:57:32 +0000

    add <WelcomeMessage />

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -52,6 +52,7 @@ const UserProfile = (props) => {
 function App() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 font-sans text-center">
+      <WelcomeMessage />
       <Header />
       <MainContent />
       <Footer />
