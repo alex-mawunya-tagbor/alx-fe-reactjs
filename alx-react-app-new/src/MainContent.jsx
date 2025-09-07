6b1bc132f137675524f558229ff7d96d1d3cf389
Alex-Mawunya-Tagbor <alexm8761@gmail.com>
2025-09-07 05:12:27 +0000

    move files from component to src

---
+++ alx-react-app-new/src/MainContent.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function MainContent() {
+  return (
+    <main style={{ padding: '20px', backgroundColor: '#eef6ff', minHeight: '200px' }}>
+      <h2 style={{ color: '#333', textAlign: 'center' }}>Welcome to My Travel Page</h2>
+      <p style={{ lineHeight: '1.6', textAlign: 'justify' }}>
+        I love to visit New York, Paris, and Tokyo.
+      </p>
+    </main>
+  );
+}
+
+export default MainContent;
---
+++ alx-react-app-new/src/UserInfo.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import UserDetails from './UserDetails';
+
+// This component no longer needs to receive props
+function UserInfo() {
+  return <UserDetails />;
+}
+
+export default UserInfo;
